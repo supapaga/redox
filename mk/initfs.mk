e7ae0892de55738d73ad9f01052e7f7c4faaffb0
supapaga <jaouen.gregory@gmail.com>
2017-10-24 18:34:35 +0200

    Update initfs.mk
    It won't compile with xargo instead of cargo :)

--- mk/initfs.mk
+++ mk/initfs.mk
@@ -1,12 +1,12 @@
 build/initfs.tag: initfs.toml
-	cd kernel && xargo clean
+	cd kernel && cargo clean
 	rm -rf build/initfs
 	mkdir -p build/initfs
 	cargo run --manifest-path installer/Cargo.toml -- $(INSTALLER_FLAGS) -c $< build/initfs/
 	touch $@
 
 build/initfs_live.tag: initfs_live.toml
-	cd kernel && xargo clean
+	cd kernel && cargo clean
 	rm -rf build/initfs_live
 	mkdir -p build/initfs_live
 	cargo run --manifest-path installer/Cargo.toml -- $(INSTALLER_FLAGS) -c $< build/initfs_live/
